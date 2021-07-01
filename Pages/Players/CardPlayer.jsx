2ed36cf377078b1c66d2260d75fd8a369cc61403
Danielgr7 <ddaniel.gr@gmail.com>
2021-07-01 11:56:44 +0300

    change

--- Pages/Players/CardPlayer.jsx
+++ Pages/Players/CardPlayer.jsx
@@ -8,11 +8,108 @@ import NotificationActions from '../../Services/NotificationActions';
 
 const { height } = Dimensions.get("screen");
 const height_logo = height * 0.3;
+
+export default function CardPlayer({ navigation, route }) {
+    const player = route.params.p;
+    const [rate, setRate] = useState(null)
+    const [age, setAge] = useState(new Date().getFullYear() - new Date(player.DateOfBirth).getFullYear())
+    const stamina = player.Stamina;
+
+    // const animate = new Animated.Value(1);
+    // const animatedStyles={
+    //     transform:[
+    //         {
+    //             scale: animate
+    //         }
+    //     ]
+    // }
+    // const startAnimation = ()=>{
+    //     Animated.timing(animate,{
+    //         toValue: 2,
+    //         duration: 1500,
+    //         useNativeDriver: true,
+    //     }).start();
+    // } 
+
+    useEffect(() => {
+        //startAnimation();
+        getRate()
+    }, [])
+    const getRate = async () => {
+        try {
+            const response = await PlayerApi.post('/GetWeightedRate', { EmailofRatingPlayer: player.Email })
+            setRate(response.data)
+        } catch (err) { console.log("this is err in player card===> " + err) }
+    }
+    return (
+        <View >
+        <View style={{paddingTop:80}}>
+        <Text style={styles.title}>Player Card</Text>
+        </View>
+        <View>
+            <ImageBackground style={styles.card_bg} source={require('../../assets/BG_PlayerCard.png')} >
+                <NotificationActions navigation={navigation} />
+
+
+                <View style={styles.content}>
+                    <View style={styles.imgAndName_View}>
+                        <View style={styles.name_View}>
+                            <Text style={[appCss.inputLabel, styles.playerName_txt]} multiline={true}>{player.FirstName}</Text>
+                            <Text style={[appCss.inputLabel, styles.playerName_txt]} multiline={true}>{player.LastName}</Text>
+                        </View>
+                        {
+                            player.PlayerPicture !== null ?
+                                <Image style={styles.player_img} source={{ uri: player.PlayerPicture }} />
+                                :
+                                <Feather name="image" size={150} color="white" style={{ paddingRight: 10 }} />
+                        }
+                    </View>
+                    <Text style={[appCss.inputLabel, styles.inputLabel_extra]}>City Live: {player.PlayerCity}</Text>
+                    <View style={styles.ageAndGender_View}>
+                        <Text style={[appCss.inputLabel, styles.inputLabel_extra, styles.gender_txt]}>Gender: {player.Gender == 0 ? "Male" : "Famele"}</Text>
+                        <Text style={[appCss.inputLabel, styles.inputLabel_extra, styles.age_txt]}>Age: {age}</Text>
+                    </View>
+
+                    <View style={{ flexDirection: 'row-reverse', justifyContent: 'center' }}>
+                        <Text style={[appCss.inputLabel, styles.inputLabel_extra]}>Stamina: </Text>
+                        <StarRating
+                            disabled={true}
+                            maxStars={5}
+                            reversed={true}
+                            rating={stamina}
+                            //selectedStar={(rating) => setStaminaStars(rating)}
+                            fullStarColor={'gold'}
+                            size={10}
+                        />
+                    </View>
+
+                    <Text style={[appCss.inputLabel, styles.inputLabel_extra]}>PreferRole:  {player.PreferredRole} </Text>
+                    <View style={appCss.rates_View}>
+                        <View style={[appCss.rate, { left: 22 }]}>
+                            <Text>Attack</Text>
+                            {rate !== null ? <Text>{rate.AttackRating}</Text> : null}
+                        </View>
+                        <View style={appCss.rate}>
+                            <Text>Defence</Text>
+                            {rate !== null ? <Text>{rate.DefenseRating}</Text> : null}
+                        </View>
+                    </View>
+                    <View style={[appCss.rate, styles.power_View]}>
+                        <Text>Power</Text>
+                        {rate !== null ? <Text>{rate.PowerRating}</Text> : null}
+                    </View>
+                </View>
+            </ImageBackground>
+        </View>
+        </View>
+    )
+}
+
 const appCss = AppCss;
 
 const styles = StyleSheet.create({
     card_bg: {
-        top: 100,
+        top: 0,
         width: '100%',
         height: '92%',
         paddingTop: 60,
@@ -29,7 +126,6 @@ const styles = StyleSheet.create({
     player_img: {
         width: 150,
         height: 150,
-        //paddingRight: 40,
         marginRight: 20,
         marginTop: 10,
         marginLeft: 10,
@@ -63,93 +159,10 @@ const styles = StyleSheet.create({
         left: 12,
         paddingBottom: 5
     },
+    title: {
+        color: 'white',
+        fontSize: 32,
+        fontWeight: 'bold',
+        alignSelf: 'center',
+      },
 });
-
-export default function CardPlayer({ navigation,route }) {
-    const player = route.params.p;
-    const [rate, setRate] = useState(null)
-    const [age, setAge] = useState(new Date().getFullYear() - new Date(player.DateOfBirth).getFullYear())
-    const stamina = player.Stamina;
-
-    // const animate = new Animated.Value(1);
-    // const animatedStyles={
-    //     transform:[
-    //         {
-    //             scale: animate
-    //         }
-    //     ]
-    // }
-    // const startAnimation = ()=>{
-    //     Animated.timing(animate,{
-    //         toValue: 2,
-    //         duration: 1500,
-    //         useNativeDriver: true,
-    //     }).start();
-    // } 
-
-    useEffect(() => {
-        //startAnimation();
-        getRate()
-    }, [])
-    const getRate = async () => {
-        try {
-            const response = await PlayerApi.post('/GetWeightedRate', { EmailofRatingPlayer: player.Email })
-            setRate(response.data)
-        } catch (err) { console.log("this is err in player card===> " + err) }
-    }
-    return (
-        <ImageBackground style={styles.card_bg} source={require('../../assets/BG_PlayerCard.png')} >
-            <NotificationActions navigation={navigation} />
-
-
-            <View style={styles.content}>
-                <View style={styles.imgAndName_View}>
-                    <View style={styles.name_View}>
-                        <Text style={[appCss.inputLabel, styles.playerName_txt]} multiline={true}>{player.FirstName}</Text>
-                        <Text style={[appCss.inputLabel, styles.playerName_txt]} multiline={true}>{player.LastName}</Text>
-                    </View>
-                    {
-                        player.PlayerPicture !== null ?
-                            <Image style={styles.player_img} source={{ uri: player.PlayerPicture }} />
-                            :
-                            <Feather name="image" size={150} color="white" style={{ paddingRight: 10 }} />
-                    }
-                </View>
-                <Text style={[appCss.inputLabel, styles.inputLabel_extra]}>City Live: {player.PlayerCity}</Text>
-                <View style={styles.ageAndGender_View}>
-                    <Text style={[appCss.inputLabel, styles.inputLabel_extra, styles.gender_txt]}>Gender: {player.Gender == 0 ? "Male" : "Famele"}</Text>
-                    <Text style={[appCss.inputLabel, styles.inputLabel_extra, styles.age_txt]}>Age: {age}</Text>
-                </View>
-
-                <View style={{ flexDirection: 'row-reverse', justifyContent: 'center' }}>
-                    <Text style={[appCss.inputLabel, styles.inputLabel_extra]}>Stamina: </Text>
-                    <StarRating
-                        disabled={true}
-                        maxStars={5}
-                        reversed={true}
-                        rating={stamina}
-                        //selectedStar={(rating) => setStaminaStars(rating)}
-                        fullStarColor={'gold'}
-                        size={10}
-                    />
-                </View>
-
-                <Text style={[appCss.inputLabel, styles.inputLabel_extra]}>PreferRole:  {player.PreferredRole} </Text>
-                <View style={appCss.rates_View}>
-                    <View style={[appCss.rate, { left: 22 }]}>
-                        <Text>Attack</Text>
-                        {rate !== null ? <Text>{rate.AttackRating}</Text> : null}
-                    </View>
-                    <View style={appCss.rate}>
-                        <Text>Defence</Text>
-                        {rate !== null ? <Text>{rate.DefenseRating}</Text> : null}
-                    </View>
-                </View>
-                <View style={[appCss.rate, styles.power_View]}>
-                    <Text>Power</Text>
-                    {rate !== null ? <Text>{rate.PowerRating}</Text> : null}
-                </View>
-            </View>
-        </ImageBackground>
-    )
-}
